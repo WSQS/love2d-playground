c825989695acf248608a74e012aee2deba28354e
WSQSylock <wsqsylock@gmail.com>
2025-02-16 21:00:11 +0800

    using cache to accelerate calculate combination

--- combination.lua
+++ combination.lua
@@ -1,14 +1,30 @@
-local combination_cache
+local combination_cache = {}
 
 function Combination(n, k)
+    if combination_cache[n]~=nil and combination_cache[n][k]~=nil then
+        return combination_cache[n][k]
+    end
     if n < k then
         return Combination(k, n)
     end
     if k == 0 or k == n then
+        if combination_cache[n] == nil then
+            combination_cache[n] = {}
+        end
+        combination_cache[n][k] = 1
         return 1
     end
     if k == 1 then
+        if combination_cache[n] == nil then
+            combination_cache[n] = {}
+        end
+        combination_cache[n][k] = n
         return n
     end
-    return Combination(n - 1, k - 1) + Combination(n - 1, k)
+    local result = Combination(n - 1, k - 1) + Combination(n - 1, k)
+    if combination_cache[n] == nil then
+        combination_cache[n] = {}
+    end
+    combination_cache[n][k] = result
+    return result
 end
