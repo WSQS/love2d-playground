5892752bcac2aba00940b2e034b21933d32ce1dd
WSQSylock <wsqsylock@gmail.com>
2025-02-16 21:12:56 +0800

    add key press for Perlin noise

--- main.lua
+++ main.lua
@@ -3,15 +3,18 @@ local v = 500
 local t = 1
 local fun = {}
 local tick
-
+local step = 1
 function love.load()
     tick = require "tick"
     Object = require "classic"
     F = require "func"
     Player = Object:extend()
     fun[0] = Function()
-    fun[1] = Sin()
-    fun[2] = PerlinNoise()
+    -- fun[1] = Sin()
+    for i = 0, step, 1 do
+        fun[#fun+1] = PerlinNoise(i)
+    end
+    step = step + 1
 end
 
 function love.update(dt)
@@ -34,9 +37,19 @@ end
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     -- love.graphics.print("Hello, World!", 100, 200) -- 打印文本
-    love.graphics.rectangle("line", x, 50, 200, 150)
+    -- love.graphics.rectangle("line", x, 50, 200, 150)
     local width, height, _ = love.window.getMode()
     for kay, value in pairs(fun) do
         value:draw(width, height, t)
+    end
+end
+
+function love.keypressed(key)
+    if key == "=" then
+        step = step + 1
+        fun[step] = PerlinNoise(step)
+    elseif key == "-" then
+        fun[step] = nil
+        step = step - 1
     end
 end
